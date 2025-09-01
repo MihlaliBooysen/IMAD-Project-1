e828ed457d1bcd12e3f37364788f45329fa5e68b
ST10476285 <ST10476285@vcconnect.edu.za>
2025-09-01 21:50:51 +0200

    My Food App

--- app/src/main/java/com/example/themealapp/MainActivity.kt
+++ app/src/main/java/com/example/themealapp/MainActivity.kt
@@ -8,7 +8,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-
+// initiating all my variables
 class MainActivity : AppCompatActivity() {
     private lateinit var edittextNamebox: EditText
     private lateinit var submitButton: Button
@@ -24,28 +24,28 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
+        } // initiating all my variables
         edittextNamebox = findViewById(R.id.edittextNamebox)
         submitButton = findViewById(R.id.submitButton)
         displayText = findViewById(R.id.displayText)
         buttonReset = findViewById(R.id.buttonReset)
 
-        submitButton.setOnClickListener {
+        submitButton.setOnClickListener { // setting my button to process the user's input
             val time = edittextNamebox.text.toString().trim().lowercase()
             var mealType = ""
-
+// Setting conditions
             if (time == "morning") {
                 mealType = "Bacon and eggs"
             } else if (time == "lunch") {
                 mealType = "Salad and a Protein shake"
             } else if (time == "lunch") {
                 mealType = "Salad and a Protein shake"
-            } else {
+            } else { // Message that shows when the user has inserted the incorrect conditions
                 mealType = "Please enter a valid time of day maybe try Morning"
             }
             displayText.text = mealType
         }
-
+// Clear text button
         buttonReset.setOnClickListener {
             edittextNamebox.text.clear()
             displayText.text = ""
